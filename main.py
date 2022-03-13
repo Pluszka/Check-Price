ff365b0815488b037e8f84da1a692ee87f1eed0b
Pliszka <karoliniakkjk@gmail.com>
2022-03-14 00:11:54 +0100

    created soup

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
+import lxml
 
 AMAZON_URL = 'https://www.amazon.com/Magic-Gathering-Innistrad-Commander-Bloodline/dp/B099YJBTNF/'\
     'ref=sr_1_3?crid=382YCNUGBQ5D&keywords=mtg%2Bcommander%2Bdeck&qid=1647208996&'\
     'sprefix=mtg%2Bcommander%2Bdeck%2Caps%2C189&sr=8-3&th=1'
 
-parameters = {
+header = {
     "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0',
     "Accept-Language": 'en-US'
 }
 
-response = requests.get(url=AMAZON_URL, params=parameters)
+response = requests.get(url=AMAZON_URL, headers=header)
 website = response.text
-print(website)
+
+soup = BeautifulSoup(website, 'lxml')
+
+print(soup.find(name="span", id="productTitle"))

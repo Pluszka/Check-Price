89a2b6e5961d7fff3645e6f8858cf644f806eb94
Pliszka <karoliniakkjk@gmail.com>
2022-03-14 21:02:44 +0100

    created connection and sending email (with bug)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import smtplib
+import os
 
-AMAZON_URL = 'https://www.amazon.com/Magic-Gathering-Innistrad-Commander-Bloodline/dp/B099YJBTNF/'\
-    'ref=sr_1_3?crid=382YCNUGBQ5D&keywords=mtg%2Bcommander%2Bdeck&qid=1647208996&'\
-    'sprefix=mtg%2Bcommander%2Bdeck%2Caps%2C189&sr=8-3&th=1'
+EMAIL = os.environ.get('EMAIL_LOGIN')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+MY_FUNDS = 36.00
+AMAZON_URL = 'https://www.amazon.com/Magic-Gathering-Innistrad-Commander-Bloodline/dp/B099YJBTNF/'
 
 header = {
     "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0',
@@ -16,4 +19,11 @@ website = response.text
 
 soup = BeautifulSoup(website, 'lxml')
 
-print(soup.find(name="span", id="productTitle"))
+current_price = soup.find(name='span', class_='a-offscreen').getText()[1:]
+
+if float(current_price) >= MY_FUNDS:
+    connection = smtplib.SMTP('smtp.gmail.com')
+    connection.starttls()
+    connection.login(user=EMAIL, password=EMAIL_PASSWORD)
+    connection.sendmail(from_addr=EMAIL, to_addrs=EMAIL,
+                        msg=f'Subject:MTG DECK ALERT\n\n Mtg commander deck is already in bargain price. Check it out!\nhttps://www.amazon.com/Magic-Gathering-Innistrad-Commander-Bloodline/dp/B099YJBTNF/')
